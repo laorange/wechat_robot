76374bc9a741d35392a6b532405e2adfb7a14be1
paul-6666 <2625835752@qq.com>
2020-08-13 21:51:09 +0800

    weather的一点点小优化

--- weather.py
+++ weather.py
@@ -1,14 +1,19 @@
 from bs4 import BeautifulSoup as bs
 import re
 import requests
-url='http://tianqi.2345.com/dongli1d/60140.htm'
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'}
-wb_data=requests.get(url,headers=headers)
-soup=bs(wb_data.text,'lxml')
-temp=soup.find_all('div',class_='real-mess')
-temp=str(list(temp)[0])
-pattern = re.compile(r'<[^>]+>',re.S)
-temp=pattern.sub('',temp)
-temp=temp.split()
+
+url = 'http://tianqi.2345.com/dongli1d/60140.htm'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'}
+wb_data = requests.get(url, headers=headers)
+soup = bs(wb_data.text, 'lxml')
+temp = soup.find_all('div', class_='real-mess')
+temp = str(list(temp)[0])
+pattern = re.compile(r'<[^>]+>', re.S)
+temp = pattern.sub('', temp)
+temp = temp.split()
 print(temp)
-print(temp[5]+','+temp[6]+','+temp[7]+temp[8])
+weather = temp[5] + ',' + temp[6] + ',' + temp[7] + temp[8]
+
+if __name__ == "__main__":
+    print(weather)
