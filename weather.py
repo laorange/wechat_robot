22cd666a2d3c08c9774f462fb026e915df16e721
etienne0519 <929211182@qq.com>
2020-08-13 21:42:53 +0800

    for test2

--- weather.py
+++ weather.py
@@ -2,10 +2,12 @@ from bs4 import BeautifulSoup as bs
 import re
 import requests
 url='http://tianqi.2345.com/dongli1d/60140.htm'
-wb_data=requests.get(url)
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'}
+wb_data=requests.get(url,headers=headers)
 soup=bs(wb_data.text,'lxml')
 temp=soup.find_all('div',class_='real-mess')
-temp=list(temp)[0]
+temp=str(list(temp)[0])
 pattern = re.compile(r'<[^>]+>',re.S)
 temp=pattern.sub('',temp)
+temp=temp.split()
 print(temp)
