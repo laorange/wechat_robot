cdea690206cc3690ccbac2b647820b0dc2f3d7b8
laorange <2625835752@qq.com>
2020-08-28 18:32:18 +0800

    优化每日任务生成方式

--- base_on_PyWeChatSpy/__main__.py
+++ base_on_PyWeChatSpy/__main__.py
@@ -9,7 +9,7 @@ from util.week import determine_week, determine_what_day, determine_date
 
 # from application.review_word.get_word import get_word
 
-start_date = '2020-08-27'
+start_date = '2020-08-29'
 start_hms = '06:00:00'
 start_time = start_date + ' ' + start_hms
 
@@ -24,7 +24,7 @@ t_next = t_start
 user_list_path = 'data/private_space/user_list.csv'
 
 
-def student_send():
+def student_send(before_term_begin=False):
     user_list = read_file2list(user_list_path)
 
     student_ls = []
@@ -33,15 +33,22 @@ def student_send():
         student_ls.append(Student(user_info_ls[0], user_info_ls[1], user_info_ls[2], user_info_ls[3], user_info_ls[4]))
 
     for student in student_ls:
-        student.get_schedule_and_send(determine_week(), determine_what_day(), determine_date())
+        if not before_term_begin:
+            student.get_schedule(determine_week(), determine_what_day())
+        if True:
+            student.send_weather(determine_week(), determine_date())
+        if not before_term_begin:
+            student.send_schedule(determine_date())
 
 
 def start():
     global t_next
     # 自用的单词复习
     try:
-        send_review_word(20)
-        do_at_sometime(lambda: send_review_word(20), time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(t_next+50400)))
+        review_word_num = 20  # 复习单词的数量
+        send_review_word(review_word_num)
+        do_at_sometime(lambda: send_review_word(review_word_num),
+                       time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(t_next + 50400)))
     except Exception as e:
         print('自用的单词复习模块出错')
         print(e)
@@ -49,11 +56,14 @@ def start():
     # 课表推送
     try:
         what_day_num = time.strftime('%w', time.localtime())
-        if determine_week() in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]:
+        if determine_week() in [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]:
             if what_day_num in ['1', '2', '3', '4', '5']:  # 周一到周五，%w是从周日开始计数
                 try:
                     print(time.strftime('%Y-%m-%d %H:%M:', time.localtime()), "start checking students' info")
-                    student_send()
+                    if determine_week() == -1:
+                        student_send(before_term_begin=True)
+                    else:
+                        student_send(before_term_begin=False)
                 except Exception as e:
                     print(time.strftime('%Y-%m-%d %H:%M:', time.localtime()), 'student_send', e)
             else:
@@ -63,23 +73,18 @@ def start():
     except Exception as e:
         print('课表推送模块出错')
         print(e)
-'''
-    t_next += 86400
-    t_next_str = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(t_next))
-    do_at_sometime(start, t_next_str)
-'''
+
+
 def restart():
     global t_next
-    for day in range(126):
+    for day in range(150):
         t_next += 86400
         t_next_str = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(t_next))
         do_at_sometime(start, t_next_str)
 
+
 def task_start():
-    do_at_sometime(start, start_time)
-
-
-# def review_word():
+    do_at_sometime(restart, start_time)
 
 
 if __name__ == "__main__":
