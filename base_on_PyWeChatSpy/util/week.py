08f27c97c306039e61d6f0a9888a898332f3edd1
laorange <2625835752@qq.com>
2020-09-19 13:31:24 +0800

    新增函数:计算指定日在本学期的第N周

--- base_on_PyWeChatSpy/util/week.py
+++ base_on_PyWeChatSpy/util/week.py
@@ -22,8 +22,18 @@ def determine_date():
     return date
 
 
+def compute_week_num(target_day: str):
+    target_day_final_str = target_day + ' 00:00:00'
+    t_target_strp = time.strptime(target_day_final_str, '%Y-%m-%d %H:%M:%S')
+    t_target = time.mktime(t_target_strp)
+    week = int((t_target - t_refer) // 604800)
+    return week
+
+
 if __name__ == "__main__":
     print(f't_refer:{t_refer}')
     print(f'week:{determine_week()}')
     print(f'what_day:{determine_what_day()}')
     print(f'date:{determine_date()}')
+
+    print(compute_week_num('2020-09-26'))
