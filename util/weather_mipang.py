cf62bee53dc441c3c72ede5328fab48f60b2324b
laorange <2625835752@qq.com>
2020-11-27 20:11:06 +0800

    优化 天气查询

--- util/weather_mipang.py
+++ util/weather_mipang.py
@@ -14,27 +14,18 @@ def get_weather():
     temp = str(list(temp)[0])
     pattern = re.compile(r'<[^>]+>', re.S)
     temp = pattern.sub('', temp)
-    return temp
-
-
-def get_weather_plus():
-    url = 'https://weather.mipang.com/tianqi-8284'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'}
-    wb_data = requests.get(url, headers=headers)
-    soup = bs(wb_data.text, 'lxml')
-    temp = soup.find_all('div', class_='row row1')
-    temp = str(list(temp)[0])
-    pattern = re.compile(r'<[^>]+>', re.S)
-    temp = pattern.sub('', temp)
     pmtemp = soup.find_all('div', class_='br br2')
     pmtemp = str(list(pmtemp)[0])
     pmtemp = pattern.sub('', pmtemp)
     pmtemp = re.sub(r'pm2.5\n*', r'空气质量:', pmtemp, count=1)
     pmtemp = re.split(r'\n', pmtemp)
+    tmtemp = soup.find_all('div', class_='tt t2 fl')
+    tmtemp = str(list(tmtemp)[0])
+    tmtemp = pattern.sub('', tmtemp)
+    temp = temp + '\n' + '今日气温：' + tmtemp.replace(' - ', '~')
     for each in pmtemp:
         if len(each) != 0:
-            if each[:4] != '空气质量':
+            if '污染危害' not in each:
                 temp = temp + '\n' + each
     return temp
 
@@ -53,5 +44,4 @@ if __name__ == "__main__":
     today_weather = Weather()
     today_weather.update_weather()
     # print(get_weather())
-    print(get_weather_plus())
-    # print(today_weather.weather)
+    print(today_weather.weather)
