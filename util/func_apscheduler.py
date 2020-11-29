91b0b9a927c4752a548b05a20f800e33b3144f72
laorange <2625835752@qq.com>
2020-11-29 22:34:19 +0800

    优化 异常处理

--- util/func_apscheduler.py
+++ util/func_apscheduler.py
@@ -3,6 +3,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.schedulers.background import BlockingScheduler
 from loguru import logger
 from util.time_util import determine_standard_time
+import traceback
 
 
 def my_job():
@@ -20,50 +21,72 @@ def do_at_sometime(func, run_time, countdown=False, if_block=False, misfire_grac
     :param run_time: 形如'2020-08-07 17:17:10' ‘%Y-%m-%d %H:%M’的时间
     :return: None
     """
-    if countdown:
-        run_time = determine_standard_time(run_time)
-    run_time_strp = time.strptime(run_time, '%Y-%m-%d %H:%M:%S')
-    t_delay = time.mktime(run_time_strp) - time.time()
-    if t_delay > 3600:
-        misfire_grace_time = int(t_delay // 3600)  # 允许1个小时产生1秒误差，不足1秒记作1秒
+    try:
+        if countdown:
+            run_time = determine_standard_time(run_time)
+        run_time_strp = time.strptime(run_time, '%Y-%m-%d %H:%M:%S')
+        t_delay = time.mktime(run_time_strp) - time.time()
+        if t_delay > 3600:
+            misfire_grace_time = int(t_delay // 3600)  # 允许1个小时产生1秒误差，不足1秒记作1秒
 
-    if t_delay > -3:  # 三秒容错
-        if if_block:
-            scheduler = BlockingScheduler()
+        if t_delay > -3:  # 三秒容错
+            if if_block:
+                scheduler = BlockingScheduler()
+            else:
+                scheduler = BackgroundScheduler()
+            scheduler.add_job(func, 'date', run_date=run_time,
+                              misfire_grace_time=misfire_grace_time, id=func.__name__, args=args)
+            logger.info(f'future task {run_time}: "{func.__name__}"')
+            scheduler.start()
         else:
-            scheduler = BackgroundScheduler()
-        scheduler.add_job(func, 'date', run_date=run_time,
-                          misfire_grace_time=misfire_grace_time, id=func.__name__, args=args)
-        logger.info(f'future task {run_time}: "{func.__name__}"')
-        scheduler.start()
-    else:
-        logger.error(f'run time miss: {run_time}: "{func.__name__}"')
+            logger.error(f'run time miss: {run_time}: "{func.__name__}"')
+    except Exception as e:
+        traceback.print_exc()
+        logger.error(e)
 
 
 def do_something_at_regular_intervals(func, interval, args=None, if_exe_right_now=True, if_block=False,
                                       misfire_grace_time=1):
-    if if_block:
-        scheduler = BlockingScheduler()
-    else:
-        scheduler = BackgroundScheduler()
-
-    if interval > 3600:
-        misfire_grace_time = int(interval // 3600)  # 允许1个小时产生1秒误差
-
-    scheduler.add_job(func, 'interval', seconds=interval,
-                      misfire_grace_time=misfire_grace_time, id=func.__name__, args=args)
-    logger.info(f'interval task ({interval}s): "{func.__name__}"')
-    if if_exe_right_now:
-        logger.info(f'interval task exe_right_now: "{func.__name__}"')
-        if isinstance(args, list):
-            if args:
-                func(args)
+    try:
+        if if_block:
+            scheduler = BlockingScheduler()
         else:
-            func()
-    scheduler.start()
+            scheduler = BackgroundScheduler()
+
+        if interval > 3600:
+            misfire_grace_time = int(interval // 3600)  # 允许1个小时产生1秒误差
+
+        scheduler.add_job(func, 'interval', seconds=interval,
+                          misfire_grace_time=misfire_grace_time, id=func.__name__, args=args)
+        logger.info(f'interval task ({interval}s): "{func.__name__}"')
+        if if_exe_right_now:
+            logger.info(f'interval task exe_right_now: "{func.__name__}"')
+            if isinstance(args, list):
+                if args:
+                    func(args)
+            else:
+                func()
+        scheduler.start()
+    except Exception as e:
+        traceback.print_exc()
+        logger.error(e)
+
+
+class Test:
+    def __init__(self):
+        self.name = 'paul'
+
+    def pr(self):
+        print(self.name)
+
+    def st(self):
+        do_at_sometime(do_something_at_regular_intervals, run_time=3, countdown=True, args=[self.pr, 3], if_block=True)
 
 
 if __name__ == "__main__":
     # do_at_sometime(my_job, -2, countdown=True, if_block=True)
     # do_something_at_regular_intervals(my_job, interval=5, if_block=True)
-    do_at_sometime(do_something_at_regular_intervals, run_time=3, countdown=True, args=[my_job, 3], if_block=True)
+
+    # do_at_sometime(do_something_at_regular_intervals, run_time=3, countdown=True, args=[, 3], if_block=True)
+    test = Test()
+    test.st()
